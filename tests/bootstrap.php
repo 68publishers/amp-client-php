5b8c3f8570aa9c8824665b691dba367d133ffc68
Tomáš Glawaty <tomasglawaty@icloud.com>
2023-11-23 05:00:37 +0100

    Tests - disabled E_DEPRECATED from vendor code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,4 +14,15 @@ if (!$loader) {
 Environment::setup();
 Environment::bypassFinals();
 
+# disable E_DEPRECATED errors from the vendor code (some nette packages before v3.1 are not fully compatible with PHP 8.1/8.2)
+$previousHandler = set_error_handler(static function (int $errNo, string $errStr, string $errFile, int $errLine) use (&$previousHandler): bool {
+    $vendor = realpath(__DIR__ . '/../vendor');
+
+    if (E_DEPRECATED === $errNo && 0 === strncmp($errFile, $vendor, strlen($vendor))) {
+        return true;
+    }
+
+    return $previousHandler($errNo, $errStr, $errFile, $errLine);
+});
+
 return $loader;
